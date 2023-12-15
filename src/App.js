c95b64cc93b7ff81fb71f190626b790a9ee1b58c
nudibranchy <frankietown@gmail.com>
2023-12-15 14:27:46 -0800

    finish onus phase 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart';
 import ProduceList from './components/ProduceList';
-import { useDispatch } from 'react-redux';
 import { populateProduce } from './store/produce';
+import { getAllCartItems } from './store/cart';
 
 function App() {
   const [showCart, setShowCart] = useState(false);
 
+  // when page opens, show list of all produce
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(populateProduce());
   }, [dispatch]);
 
+  // when first item added to cart, show cart side bar
+  const cart = useSelector(state => state.cart);
+  useEffect(() => {
+    if (Object.keys(cart) > 0) setShowCart(true);
+  }, [cart])
 
   return (
     <>
